bd7549331185982afa82885874b7b089eac6e9fb
patgarcia <commonzenmedia@gmail.com>
2021-05-15 13:14:49 -0400

    Added listen method to app

--- app.js
+++ app.js
@@ -15,3 +15,6 @@ app.use(express.static(__dirname + 'public/'));
 // Controllers
 app.use(require('./controllers'))
 
+// Listen
+const port=4000;
+app.listen(port, () => console.log(`app running on port ${port}`));
