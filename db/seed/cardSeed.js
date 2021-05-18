da679a6e372c9aba69031310282da4c6c34a8a39
latham <lathamstevenjohnson@gmail.com>
2021-05-18 13:21:35 -0400

    updated card seeding

--- db/seed/cardSeed.js
+++ db/seed/cardSeed.js
@@ -1,8 +1,16 @@
 const Card = require('../../models/card')
-const seedData = require('./cards.json')
+const locations = require('./locationSeed')
+const images = require('./imagesSeed')
 
-Card.deleteMany({})
-.then(() => {return Card.insertMany(seedData)})
-.then(console.log)
-.then(console.error)
-.finally(() => {process.exit()})
+locations.map(locale, key => {
+    Card.create({
+        image: images[images.length%key].hq_image_url,
+        owner: `ronnieross711`,
+        hashtag: '',
+        location: locale,
+        vote_tally: 0,
+        represents_collection: false,
+        collectionID: ''
+    })
+})
+
