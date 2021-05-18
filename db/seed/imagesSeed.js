73952b5104a30c5607e8fb43e5929a3841598474
latham <lathamstevenjohnson@gmail.com>
2021-05-18 19:40:46 -0400

    cleaned up seed file

--- db/seed/imagesSeed.js
+++ db/seed/imagesSeed.js
@@ -3,11 +3,7 @@ const seedData = require("./images.json");
 
 // clears collection and adds data
 Image.deleteMany({})
-  .then(() => {
-    return Image.insertMany(seedData);
-  })
+  .then(() => {return Image.insertMany(seedData);})
   .then(console.log) // if successful
   .catch(console.error) // if there is an error
-  .finally(() => {
-    process.exit();
-  });
+  .finally(() => {process.exit();});
