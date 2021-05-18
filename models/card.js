2d7d1c7d4745c5e8ddff99f9d7ca726d80ac55b9
latham <lathamstevenjohnson@gmail.com>
2021-05-18 11:52:49 -0400

    card model and seed files

--- models/card.js
+++ models/card.js
@@ -1,1 +1,27 @@
 const mongoose = require('../db/connection');
+const Schema = mongoose.Schema;
+
+const cardSchema = new Schema(
+    {
+        image: {
+            type: Schema.Types.ObjectId, 
+            ref: 'Image'
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        hashtag: String,
+        location: {
+            type: Schema.Types.ObjectId,
+            ref: 'Location'
+        },
+        vote_tally: Number,
+        represents_collection: Boolean,
+        collectionID: String,
+    },
+    {timestamps: true}
+)
+
+const Card = mongoose.model('Card', cardSchema);
+module.exports = Card
