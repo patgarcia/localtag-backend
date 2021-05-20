383874dc741d81cc3be8958d038e9aa465709ad5
patgarcia <commonzenmedia@gmail.com>
2021-05-20 09:32:53 -0400

    Installed express-session

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
 
 // Options
 app.use(helmet())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(session());
 
 // Routes
 app.use(express.static(__dirname + 'public/'));
